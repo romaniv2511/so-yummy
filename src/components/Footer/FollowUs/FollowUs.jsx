6d0cb143625d1f94add112811ee9264d5efe4f50
Misha Panivnyk <panivnykm@gmail.com>
2023-03-31 16:47:14 +0300

    add footer/FollowUs

---
+++ src/components/Footer/FollowUs/FollowUs.jsx
@@ -0,0 +1,29 @@
+import sprite from '../../../images/sprite.svg';
+import { FollowUsContainer, Link } from './FollowUs.styled';
+
+export const FollowUs = () => {
+  return (
+    <FollowUsContainer>
+      <Link to="https://www.facebook.com/">
+        <svg width={20} height={20}>
+          <use href={sprite + '#icon-facebook'}></use>
+        </svg>
+      </Link>
+      <Link to="https://www.youtube.com/">
+        <svg width={20} height={20}>
+          <use href={sprite + '#icon-youtube'}></use>
+        </svg>
+      </Link>
+      <Link to="https://twitter.com/">
+        <svg width={20} height={20}>
+          <use href={sprite + '#icon-twitter'}></use>
+        </svg>
+      </Link>
+      <Link to="https://www.instagram.com/">
+        <svg width={20} height={20}>
+          <use href={sprite + '#icon-instagram'}></use>
+        </svg>
+      </Link>
+    </FollowUsContainer>
+  );
+};
