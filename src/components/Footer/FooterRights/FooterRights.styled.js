fe4e9627c06768c66ae3d8d49a628a08b6f2ccd8
Misha Panivnyk <panivnykm@gmail.com>
2023-03-31 16:47:47 +0300

    add footer/FooterRights

---
+++ src/components/Footer/FooterRights/FooterRights.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const FooterRightsBg = styled.div`
+  text-align: center;
+  width: 100%;
+  padding: 28px 0;
+  background-color: transparent;
+  span {
+    margin-left: 14px;
+    font-weight: 400;
+  }
+
+  p {
+    font-size: 10px;
+    line-height: 1;
+    font-style: normal;
+    font-weight: 500;
+    letter-spacing: -0.01em;
+    color: #22252a;
+    opacity: 0.5;
+  }
+`;
