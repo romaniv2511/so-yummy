85763c993a7f70563bc29f2bec7427699b0627d9
Misha Panivnyk <panivnykm@gmail.com>
2023-03-31 16:50:05 +0300

    add Footer in SharedLayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,17 +2,18 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { GlobalStyle } from '../GlobalStyle';
 import { AppBar } from '../AppBar/AppBar';
+import { Footer } from 'components/Footer/Footer';
 
-
-
-const SharedLayout = ()=> {
+const SharedLayout = () => {
   return (
     <>
-      <GlobalStyle/>
-      <AppBar/>
+      <GlobalStyle />
+      <AppBar />
       <Suspense fallback={<p>Loading...</p>}>
         <Outlet />
       </Suspense>
-    </>)
-}
+      <Footer />
+    </>
+  );
+};
 export default SharedLayout;
