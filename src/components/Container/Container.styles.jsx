3e83fa0a2c0ee1718f13a5304904f03cc4e22300
Alexandr Romaniv <alexandr.romaniv@mtrading.com>
2023-04-01 00:09:28 +0300

    update container

--- src/components/Container/Container.styles.jsx
+++ src/components/Container/Container.styles.jsx
@@ -3,5 +3,17 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   margin-left: auto;
   margin-right: auto;
+  padding-left: 16px;
+  padding-right: 16px;
   max-width: ${p=>p.theme.breakpoints.mob};
+  @media screen and (min-width: ${p=>p.theme.breakpoints.tab}){
+    width: ${p=>p.theme.breakpoints.tab};
+    padding-left: 32px;
+    padding-right: 32px;
+  };
+  @media screen and (min-width: ${p=>p.theme.breakpoints.desk}){
+    width: ${p=>p.theme.breakpoints.desk};
+    padding-left: 100px;
+    padding-right: 100px;
+  }
 `
