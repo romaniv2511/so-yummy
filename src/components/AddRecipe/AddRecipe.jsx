e0aa174943449e35193201323a2f0cb1ae5a594d
Yevhen <leshchenkoyevhen@gmail.com>
2023-03-31 10:46:33 -0400

    init start

---
+++ src/components/AddRecipe/AddRecipe.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const AddRecipe = () => {
+  return (
+    <div>
+      <p>
+        Зверстати компоненти що знаходяться на сторінці AddRecipePage (мобільна,
+        планшет та десктопна версії): 1. MainTitle - універсальний компонент, що
+        відмальовує заголовок і використовується на різних сторінках застосунку
+        2. AddRecipeForm 3. FollowUs 4. PopularRecipe
+      </p>
+    </div>
+  );
+};
+
+export default AddRecipe;
+
+// Зверстати компоненти що знаходяться на сторінці AddRecipePage (мобільна,
+//         планшет та десктопна версії): 1. MainTitle - універсальний компонент, що
+//         відмальовує заголовок і використовується на різних сторінках застосунку
+//         2. AddRecipeForm 3. FollowUs 4. PopularRecipe
