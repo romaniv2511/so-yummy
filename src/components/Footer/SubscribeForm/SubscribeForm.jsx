cd9777c0ba332227c006b58197e401309be45490
Misha Panivnyk <panivnykm@gmail.com>
2023-04-01 12:30:03 +0300

    add footer-style-desc/SubscribeForm

--- src/components/Footer/SubscribeForm/SubscribeForm.jsx
+++ src/components/Footer/SubscribeForm/SubscribeForm.jsx
@@ -1,12 +1,28 @@
 import { Formik } from 'formik';
-import { Form, FormInput, FormBtn, FormWrap } from './SubscribeForm.styled';
+import {
+  Form,
+  FormInput,
+  FormBtn,
+  FormWrap,
+  FormWrapText,
+} from './SubscribeForm.styled';
 import sprite from '../../../img/sprite.svg';
+import MediaQuery from 'react-responsive';
 
 export const SubscribeForm = () => {
   return (
     <>
       <Formik initialValues={{ email: `` }}>
         <Form>
+          <MediaQuery minWidth={1440}>
+            <FormWrapText>
+              <h2>Subscribe to our Newsletter</h2>
+              <p>
+                Subscribe up to our newsletter. Be in touch with latest news and
+                special offers, etc.
+              </p>
+            </FormWrapText>
+          </MediaQuery>
           <FormWrap>
             <FormInput type="email" placeholder="Enter your email address" />
             <svg>
