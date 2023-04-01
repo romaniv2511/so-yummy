e46d5a54825db066909651e287daad4cc4d4ed2f
Alexandr Romaniv <alexandr.romaniv@mtrading.com>
2023-04-01 14:38:48 +0300

    update MainPage

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,13 +1,6 @@
-import Hero from 'components/Hero/Hero';
-import { BackgroundHero } from './WelcomePage.styled';
-import { PreviewCategories } from 'components/PreviewCategories/PreviewCategories';
-
 const WelcomePage = () => {
   return (
-    <BackgroundHero>
-      <Hero />
-      <PreviewCategories />
-    </BackgroundHero>
+    <p>Welcome</p>
   );
 };
 export default WelcomePage;
