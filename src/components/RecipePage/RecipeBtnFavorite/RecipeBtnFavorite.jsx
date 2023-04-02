f12e37eb0ac5cd8fc320d17ae0c83b9b13e91c29
Misha Panivnyk <panivnykm@gmail.com>
2023-04-02 17:31:37 +0300

    add components/RecipeBtnFavorite

---
+++ src/components/RecipePage/RecipeBtnFavorite/RecipeBtnFavorite.jsx
@@ -0,0 +1,5 @@
+import { RecipeBtn } from './RecipeBtnFavorite.styled';
+
+export const RecipeBtnFavorite = () => {
+  return <RecipeBtn>Add to favorite recipes</RecipeBtn>;
+};
---
+++ src/components/RecipePage/RecipeBtnFavorite/RecipeBtnFavorite.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const RecipeBtn = styled.button`
+  border: 1px solid ${({ theme }) => theme.color.accent};
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 1.5;
+  color: ${({ theme }) => theme.color.textRights};
+  padding: 10px 18px;
+  cursor: pointer;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1) 0s;
+  background-color: transparent;
+  outline: none;
+  border-radius: 30px 80px;
+  text-align: center;
+  :hover,
+  :focus {
+    background: ${({ theme }) => theme.color.accent};
+    color: ${({ theme }) => theme.color.textSecondary};
+  }
+`;
