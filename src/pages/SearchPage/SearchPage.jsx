73e268556f054e702eae9e06f8e3dd9bca6b4b38
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-04-05 19:22:01 +0300

    add searchForm

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,26 +1,42 @@
 import { MainPageTitle } from 'components/MainPageTitle/MainPageTitle';
 import { PagesWrapper } from 'components/PagesWrapper/PagesWrapper';
 import { SearchBar } from 'components/SearchBar/SearchBar';
-import { useLocation } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { SearchedRecipesList } from 'components/SearchedRecipesList/SearchedRecipesList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const SearchPage = () => {
-  const { state } = useLocation();
   const [searchList, setSearchList] = useState([]);
-  if (state) {
-    console.log(state.searchText);
-  }
+  const [searchParams, setSearchParams] = useSearchParams();
+  const valueName = 'query'; // "ingredient"
+  const value = searchParams.get(`${valueName}`) ?? '';
 
-  const handleSearch = e => {
-    e.preventDefault();
-    setSearchList(e.target.value);
+  const handleSearch = (searchText, actions) => {
+    const nextParams =
+      searchText !== '' ? { [valueName]: searchText.searchText } : {};
+    setSearchParams(nextParams);
+    setSearchList([]);
+    actions.resetForm({ values: { searchText: '' } });
   };
+
+  const getSearchList = async (value, valueName) => {
+    const response = await axios.get(
+      `https://soyummy-tw3y.onrender.com/api/v1//search?page=1&limit=20&query=${value}&type=${valueName}`
+    );
+    const { data } = response.data;
+    setSearchList(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getSearchList(value, valueName);
+  }, [value, valueName]);
 
   return (
     <PagesWrapper>
       <MainPageTitle title="Search" />
-      <SearchBar handleSearch={handleSearch} />
+      <SearchBar value={value} handleSearch={handleSearch} />
       <SearchedRecipesList searchList={searchList} />
     </PagesWrapper>
   );
