ca3f0a6b64c7eb73b95965cc8428bc89c14ab370
Yevhen <leshchenkoyevhen@gmail.com>
2023-04-05 17:26:25 -0400

    add render pop recipes

--- src/components/AddRecipe/PopularRecipe/PopularRecipe.jsx
+++ src/components/AddRecipe/PopularRecipe/PopularRecipe.jsx
@@ -1,38 +1,56 @@
+import axios from 'axios';
+import { Loader } from 'components/Loader/Loader';
+import { ErrorMessage } from 'components/PreviewCategories/PreviewCategories.styled';
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
+
 export const PopularRecipe = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    const popularRecipes = async () => {
+      try {
+        const response = await axios.get(
+          'https://soyummy-tw3y.onrender.com/api/v1/popular-recipes'
+        );
+        const { data } = response.data;
+
+        if (response) {
+          setLoading(false);
+          setRecipes(data);
+        }
+        console.log(data);
+      } catch {
+        setError('Failed to fetch');
+        setRecipes([]);
+      }
+    };
+    popularRecipes();
+  }, []);
+
   return (
     <div>
       <h2>Popular recipe</h2>
       <ul>
-        <li>
-          <img src="" alt="" />
-          <h3>Banana Pancakes</h3>
-          <p>
-            In a bowl, mash the banana with a fork until it resembles a thick
-            purée...
-          </p>
-        </li>
-        <li>
-          <img src="" alt="" />
-          <h3>Squash linguine</h3>
-          <p>
-            Pasta is a type of food typically made from an unleavened dough of
-            wheat flour...
-          </p>
-        </li>
-        <li>
-          <img src="" alt="" />
-          <h3>Baked salmon</h3>
-          <p>Cook in boiling salted water for 10 mins...</p>
-        </li>
-        <li>
-          <img src="" alt="" />
-          <h3>Sugar Pie</h3>
-          <p>
-            Sugar pie is a dessert in northern French and Belgiancuisine, where
-            it is called tarte...
-          </p>
-        </li>
+        {recipes.map(({ _id, title, description, preview }) => (
+          <li key={_id}>
+            <Link to={`/movies/${_id}`} state={{ from: location }}>
+              <img src={preview} alt="recipe-appearance" />
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </Link>
+          </li>
+        ))}
       </ul>
+      {error && !loading && (
+        <ErrorMessage>Something wrong! Reload the page...</ErrorMessage>
+      )}
+      {loading && <Loader />}
     </div>
   );
 };
