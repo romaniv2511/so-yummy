b59317d1407c32e28bdf6b02c305a8e886e04a43
Misha Panivnyk <panivnykm@gmail.com>
2023-04-06 20:28:03 +0300

    fix

--- src/utils/formikColors.js
+++ src/utils/formikColors.js
@@ -3,11 +3,8 @@ export const getColor = (
   values,
   defaultColor = 'rgba(255, 255, 255, 0.8)'
 ) => {
-  if (
-    errors === 'Your password is little secure. Add a number!' ||
-    errors === 'Your password is little secure. Add a letter!'
-  ) {
-    return '#F6C23E';
+  if (errors) {
+    return '#E74A3B';
   }
   return values ? (errors && '#E74A3B') || '#3CBC81' : defaultColor;
 };
