b04d02ab0218f270cc4f86fae0069c3d2c8f16e6
Anastasiia Melnyk <nastaisha1988@gmail.com>
2023-04-07 19:53:02 +0300

    fixed favorite

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,12 +3,14 @@ import {
   selectIsLoggedIn,
   selectIsRefreshing,
   selectUser,
+  selectToken,
 } from 'redux/auth/authSelectors';
 
 export const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
   const user = useSelector(selectUser);
+  const token = useSelector(selectToken);
 
-  return { isLoggedIn, isRefreshing, user };
+  return { isLoggedIn, isRefreshing, user, token };
 };
---
+++ src/redux/favorites/favoritesSelectors.js
@@ -0,0 +1,5 @@
+export const selectIsLoading = state => state.favorites.isLoading;
+
+export const selectError = state => state.favorites.error;
+
+export const selectFavorites = state => state.favorites.items;
