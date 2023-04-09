d7e0e5bf00a30c2229338655b0ea1aeb072d6873
Yevhen <leshchenkoyevhen@gmail.com>
2023-04-09 09:43:09 -0400

    add changes

--- src/components/AddRecipe/AddRecipeForm/AddRecipeForm.jsx
+++ src/components/AddRecipe/AddRecipeForm/AddRecipeForm.jsx
@@ -12,8 +12,8 @@ import { RecipePreparationFields } from '../RecipePreparationFields/RecipePrepar
 import { nanoid } from 'nanoid';
 
 const initialValues = {
-  title: 'chicken',
-  description: 'description chicken',
+  title: '',
+  description: '',
   category: 'Breakfast',
   time: '40 min',
   ingredients: '',
@@ -34,7 +34,7 @@ export const AddRecipeForm = () => {
   };
 
   const handleChange = event => {
-    console.log(event.target);
+    // console.log(event.target);
     const { name, value } = event.target;
     setDescriptionFields(prevState => ({ ...prevState, [name]: value }));
   };
