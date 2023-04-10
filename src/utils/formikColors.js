e12e89bea7df5f865ce77f03a3b388f8513f6073
Misha Panivnyk <panivnykm@gmail.com>
2023-04-10 21:10:55 +0300

    add formik colors

--- src/utils/formikColors.js
+++ src/utils/formikColors.js
@@ -3,8 +3,11 @@ export const getColor = (
   values,
   defaultColor = 'rgba(255, 255, 255, 0.8)'
 ) => {
-  if (errors) {
-    return '#E74A3B';
+  if (
+    errors === 'Your password is little secure. Add a number!' ||
+    errors === 'Your password is little secure. Add a letter!'
+  ) {
+    return '#F6C23E';
   }
   return values ? (errors && '#E74A3B') || '#3CBC81' : defaultColor;
 };
