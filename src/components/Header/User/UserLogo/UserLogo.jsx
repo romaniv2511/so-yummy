e105cc8d5267905748209d1587b97ede88bde834
Alexandr Romaniv <alexandr.romaniv@mtrading.com>
2023-04-11 15:02:19 +0300

    fix user avatar

--- src/components/Header/User/UserLogo/UserLogo.jsx
+++ src/components/Header/User/UserLogo/UserLogo.jsx
@@ -1,4 +1,4 @@
-// import userAvatar from 'img/avatar.png';
+import userAvatar from 'img/avatar.png';
 import { ButtonUser, UserLogoBox, UserName } from './UserLogo.styled';
 import { useAuth } from '../../../../hooks/useAuth';
 
@@ -9,7 +9,7 @@ export const UserLogo = ({ toggleDashboard, pageMain }) => {
     <UserLogoBox>
       <ButtonUser type="button" onClick={toggleDashboard}>
 
-        <img src={user?.avatar || ''} alt="user" />
+        <img src={user?.avatar || userAvatar} alt="user" />
         <UserName main={pageMain}>{user?.name ||  'user'}</UserName>
 
       </ButtonUser>
