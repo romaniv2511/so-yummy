c0b2e335b605ec7098baa36a7c40e0a2865fc64a
Yevhen <leshchenkoyevhen@gmail.com>
2023-04-12 10:53:24 -0400

    fix measure

--- src/components/AddRecipe/RecipeIngredientsFields/RecipeIngredientsFields.jsx
+++ src/components/AddRecipe/RecipeIngredientsFields/RecipeIngredientsFields.jsx
@@ -41,6 +41,7 @@ export const RecipeIngredientsFields = ({
     values[index][event.target.name] = event.target.value;
     setInputFields(values);
     setActiveInputIndex(index);
+    onSetValue(inputFields);
     if (event.target.name !== 'measure') {
       updateQueryString(event);
     }
